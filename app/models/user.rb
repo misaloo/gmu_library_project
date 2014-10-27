2d0ec4463bbe05a4c70845ba7ddb8112a03e5a42
chrisdedmonds <chris.edmonds128@gmail.com>
2014-10-27 02:16:59 -0400

    tweak

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_secure_password
-  has_many :books, through: :reservations, dependent: :destroy
+  has_many :books, through: :reservations
 end
